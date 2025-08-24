2c13263fb5ecb6493244657128f59e52f813266d
Mintu <pmk80028@gmail.com>
2025-08-24 19:47:05 +0530

    shwo user list after the show user button click

---
+++ index.js
@@ -0,0 +1,11 @@
+const show_user_btn = document.getElementById("show_user_btn");
+ const userList = document.querySelector('.list');
+
+ show_user_btn.addEventListener('click', () => {
+    userList.style.display = 'block'
+ })
+
+
+
+
+
